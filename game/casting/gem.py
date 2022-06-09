939133c94a93b2480819943afcf4a49784acebe3
cloggerish <104546948+cloggerish@users.noreply.github.com>
2022-06-09 01:44:10 -0600

    Added Gem Class

--- game/casting/gem.py
+++ game/casting/gem.py
@@ -1,13 +1,28 @@
-#Class Gem(Actor)
-"""
-An item of value. 
+from game.casting.actor import Actor
 
-The responsibility of a gem is to provide a point value.
+class Gem(Actor):
 
-Attributes:
-    _value (int): An integer value.
-"""
+    """An item of value. 
 
-#__init__
-    #pull in all other attributes from Actor class
-#get_value
+    The responsibility of a gem is to provide a point value.
+
+    Attributes:
+        _value (int): An integer value."""
+
+    def __init__(self):
+
+        """Contructs a new Gem
+        Pulls attributes and methods from Actor class """
+
+        super().__init__()
+        self._value = 1
+
+    def get_value(self):
+
+        """Gets the gem's value
+        
+        Returns:
+            Int: The gem's value"""
+
+        return self._value
+
