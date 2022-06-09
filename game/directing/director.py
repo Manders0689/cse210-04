2e1a25f707440615e70a7a2bb6ea818de9c0c470
Manders0689 <Mandy.beck05@gmail.com>
2022-06-08 19:42:10 -0600

    Added Cast

--- game/directing/director.py
+++ game/directing/director.py
@@ -1,23 +1,24 @@
-#Director Class
+class Director:
 
-"""A person who directs the game. 
+    """A person who directs the game. 
 
-The responsibility of a Director is to control the sequence of play.
+    The responsibility of a Director is to control the sequence of play.
 
-Attributes:
-_keyboard_service (KeyboardService): For getting directional input.
-_video_service (VideoService): For providing video output.
-"""
+    Attributes:
+    _keyboard_service (KeyboardService): For getting directional input.
+    _video_service (VideoService): For providing video output.
+    """
 
 # __init__
 
-# start_game
-
-# _get_inputs
+def start_game(self):
+    pass
+        
+# _get_inputs()
 
 # _do_updates
 
 # _do_outputs
 
 
-#store/pdate/outputs total game points
+#store/update/outputs total game points
